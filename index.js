57523724f19242f984b59d233f59c152f5263f16
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-02-25 19:38:14 -0700

    Add requests to index.js

--- index.js
+++ index.js
@@ -17,3 +17,22 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : true} ))
 app.use(cors())
+
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await knex('users').select()
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
+
+app.get('/messages', async (req, res) => {
+  try {
+    const messages = await knex('messages').select()
+    res.status(200).json(messages)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+})
